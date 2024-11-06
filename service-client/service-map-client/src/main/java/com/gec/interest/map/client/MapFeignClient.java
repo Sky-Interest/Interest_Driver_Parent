4b445b22113f5baf9c11b178c87f36873fdd98d9
Sky_Interest <841102500@qq.com>
2024-11-06 11:20:03 +0800

    4.2.3代驾：预估订单数据 预估驾驶线路 Feign接口

--- service-client/service-map-client/src/main/java/com/gec/interest/map/client/MapFeignClient.java
+++ service-client/service-map-client/src/main/java/com/gec/interest/map/client/MapFeignClient.java
@@ -1,9 +1,23 @@
 package com.gec.interest.map.client;
 
+import com.gec.interest.common.result.Result;
+import com.gec.interest.model.form.map.CalculateDrivingLineForm;
+import com.gec.interest.model.vo.map.DrivingLineVo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "service-map")
 public interface MapFeignClient {
+
+    /**
+     * 计算驾驶线路
+     *
+     * @param calculateDrivingLineForm
+     * @return
+     */
+    @PostMapping("/map/calculateDrivingLine")
+    Result<DrivingLineVo> calculateDrivingLine(@RequestBody CalculateDrivingLineForm calculateDrivingLineForm);
 
 
 }
