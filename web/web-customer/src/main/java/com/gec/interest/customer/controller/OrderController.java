05fda3642e573624f75e8f75b0e4b8758ab43d70
Sky_Interest <841102500@qq.com>
2024-11-06 10:35:48 +0800

    4.1.1代驾：预估订单数据 查找乘客端当前订单

--- web/web-customer/src/main/java/com/gec/interest/customer/controller/OrderController.java
+++ web/web-customer/src/main/java/com/gec/interest/customer/controller/OrderController.java
@@ -1,7 +1,12 @@
 package com.gec.interest.customer.controller;
 
+import com.gec.interest.common.login.InterestLogin;
+import com.gec.interest.common.result.Result;
+import com.gec.interest.model.vo.order.CurrentOrderInfoVo;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderController {
 
-
+    @Operation(summary = "查找乘客端当前订单")
+    @InterestLogin
+    @GetMapping("/searchCustomerCurrentOrder")
+    public Result<CurrentOrderInfoVo> searchCustomerCurrentOrder() {
+        CurrentOrderInfoVo currentOrderInfoVo = new CurrentOrderInfoVo();
+        currentOrderInfoVo.setIsHasCurrentOrder(false);
+        return Result.ok(currentOrderInfoVo);
+    }
 }
 
