c6360441cdbd6821adb4e94ab60f97634f31c429
Sky_Interest <841102500@qq.com>
2024-11-07 14:32:47 +0800

    5.3.1乘客下单（一） 司机端查询订单状态 司机端接口

--- web/web-driver/src/main/java/com/gec/interest/driver/controller/OrderController.java
+++ web/web-driver/src/main/java/com/gec/interest/driver/controller/OrderController.java
@@ -1,7 +1,14 @@
 package com.gec.interest.driver.controller;
 
+import com.gec.interest.common.login.InterestLogin;
+import com.gec.interest.common.result.Result;
+import com.gec.interest.driver.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderController {
+    @Autowired
+    OrderService orderService;
 
+    @Operation(summary = "查询订单状态")
+    @InterestLogin
+    @GetMapping("/getOrderStatus/{orderId}")
+    public Result<Integer> getOrderStatus(@PathVariable Long orderId) {
+        return Result.ok(orderService.getOrderStatus(orderId));
+    }
 
 }
 
--- web/web-driver/src/main/java/com/gec/interest/driver/service/impl/OrderServiceImpl.java
+++ web/web-driver/src/main/java/com/gec/interest/driver/service/impl/OrderServiceImpl.java
@@ -1,13 +1,21 @@
 package com.gec.interest.driver.service.impl;
 
 import com.gec.interest.driver.service.OrderService;
+import com.gec.interest.order.client.OrderInfoFeignClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    OrderInfoFeignClient orderInfoFeignClient;
 
+    @Override
+    public Integer getOrderStatus(Long orderId) {
+        return orderInfoFeignClient.getOrderStatus(orderId).getData();
+    }
 
 }
