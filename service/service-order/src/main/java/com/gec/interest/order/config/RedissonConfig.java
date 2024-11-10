7f8afce4b2550d9a757f326e52a90e115cfb35d7
Sky_Interest <841102500@qq.com>
2024-11-10 17:10:24 +0800

    8.2.6代驾：司机抢单 司机抢单 使用Redission解决分布式锁

---
+++ service/service-order/src/main/java/com/gec/interest/order/config/RedissonConfig.java
@@ -0,0 +1,50 @@
+package com.gec.interest.order.config;
+
+import com.alibaba.nacos.common.utils.StringUtils;
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * redisson配置信息
+ */
+@Data
+@Configuration
+@ConfigurationProperties("spring.data.redis")
+public class RedissonConfig {
+
+    private String host;
+
+    private String password;
+
+    private String port;
+
+    private int timeout = 3000;
+    private static String ADDRESS_PREFIX = "redis://";
+
+
+    /**
+     * 自动装配
+     */
+    @Bean
+    RedissonClient redissonSingle() {
+        //创建Redisson的配置对象Config
+        Config config = new Config();
+        if (!StringUtils.hasText(host)) {
+            throw new RuntimeException("host is  empty");
+        }
+        SingleServerConfig serverConfig = config.useSingleServer()
+                .setAddress(ADDRESS_PREFIX + this.host + ":" + port)
+                .setTimeout(this.timeout);
+        if (StringUtils.hasText(this.password)) {
+            serverConfig.setPassword(this.password);
+        }
+        //创建Redisson的客户端对象
+        return Redisson.create(config);
+    }
+}
