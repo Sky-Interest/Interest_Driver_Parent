1b065f7603dc599aa46da7749ada2e9a00a06bca
Sky_Interest <841102500@qq.com>
2024-11-16 15:15:28 +0800

    13.2.5代驾：订单支付 微信支付(个人账号无法实现) 支付结果通知

--- service/service-payment/src/main/java/com/gec/interest/payment/service/WxPayService.java
+++ service/service-payment/src/main/java/com/gec/interest/payment/service/WxPayService.java
@@ -2,9 +2,14 @@ package com.gec.interest.payment.service;
 
 import com.gec.interest.model.form.payment.PaymentInfoForm;
 import com.gec.interest.model.vo.payment.WxPrepayVo;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
 
 public interface WxPayService {
 
 
     WxPrepayVo createWxPayment(PaymentInfoForm paymentInfoForm);
+
+    Map wxnotify(HttpServletRequest request);
 }
--- service/service-payment/src/main/java/com/gec/interest/payment/service/impl/WxPayServiceImpl.java
+++ service/service-payment/src/main/java/com/gec/interest/payment/service/impl/WxPayServiceImpl.java
@@ -1,9 +1,13 @@
 package com.gec.interest.payment.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.gec.interest.common.constant.MqConst;
 import com.gec.interest.common.execption.interestException;
 import com.gec.interest.common.result.ResultCodeEnum;
+import com.gec.interest.common.service.RabbitService;
+import com.gec.interest.common.util.RequestUtils;
 import com.gec.interest.model.entity.payment.PaymentInfo;
 import com.gec.interest.model.form.payment.PaymentInfoForm;
 import com.gec.interest.model.vo.payment.WxPrepayVo;
@@ -11,14 +15,21 @@ import com.gec.interest.payment.config.WxPayV3Properties;
 import com.gec.interest.payment.mapper.PaymentInfoMapper;
 import com.gec.interest.payment.service.WxPayService;
 import com.wechat.pay.java.core.RSAAutoCertificateConfig;
+import com.wechat.pay.java.core.notification.NotificationParser;
+import com.wechat.pay.java.core.notification.RequestParam;
+import com.wechat.pay.java.service.partnerpayments.jsapi.model.Transaction;
 import com.wechat.pay.java.service.payments.jsapi.JsapiServiceExtension;
 import com.wechat.pay.java.service.payments.jsapi.model.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -30,6 +41,8 @@ public class WxPayServiceImpl implements WxPayService {
     RSAAutoCertificateConfig rsaAutoCertificateConfig;
     @Autowired
     WxPayV3Properties wxPayV3Properties;
+    @Autowired
+    private RabbitService rabbitService;
 
     @Override
     public WxPrepayVo createWxPayment(PaymentInfoForm paymentInfoForm) {
@@ -83,5 +96,65 @@ public class WxPayServiceImpl implements WxPayService {
         } catch (Exception e) {
             throw new interestException(ResultCodeEnum.WX_CREATE_ERROR);
         }
+    }
+    @Transactional
+
+//微信支付响应后的异步处理的业务方法
+    @Override
+    public Map<String, Object> wxnotify(HttpServletRequest request) {
+        //1、获取回调通知的请求参数【校验签名、解密】
+        //1.回调通知的验签与解密\从request头信息获取参数
+        //HTTP 头 Wechatpay-Signature\ Wechatpay-Nonce\Wechatpay-Timestamp\Wechatpay-Serial\ Wechatpay-Signature-Type
+        //HTTP 请求体 body。切记使用原始报文，不要用 JSON 对象序列化后的字符串，避免验签的 body 和原文不一致。
+        String wechatPaySerial = request.getHeader("Wechatpay-Serial");
+        String nonce = request.getHeader("Wechatpay-Nonce");
+        String timestamp = request.getHeader("Wechatpay-Timestamp");
+        String signature = request.getHeader("Wechatpay-Signature");
+        String requestBody = RequestUtils.readData(request);
+        log.info("wechatPaySerial：{}", wechatPaySerial);
+        log.info("nonce：{}", nonce);
+        log.info("timestamp：{}", timestamp);
+        log.info("signature：{}", signature);
+        log.info("requestBody：{}", requestBody);
+        //2、构建请求参数
+        RequestParam requestParam = new RequestParam.Builder()
+                .serialNumber(wechatPaySerial)
+                .nonce(nonce)
+                .signature(signature)
+                .timestamp(timestamp)
+                .body(requestBody)
+                .build();
+        //提供一个消息的解析器
+        NotificationParser parser = new NotificationParser(rsaAutoCertificateConfig);
+        //3、调用支付处理的业务逻辑代码【更新支付数据、更新订单状态....】
+        Transaction transaction = parser.parse(requestParam, Transaction.class);
+        log.info("成功解析：{}", JSONObject.toJSONString(transaction));
+        //判断交易是否成功
+        if (null != transaction && transaction.getTradeState() == Transaction.TradeStateEnum.SUCCESS) {
+            //5.处理支付成功后的业务实现
+            this.handlePayment(transaction);
+        }
+        //保留数据返回
+        return null;
+    }
+
+    public void handlePayment(Transaction transaction) {
+        PaymentInfo paymentInfo = paymentInfoMapper.selectOne(new LambdaQueryWrapper<PaymentInfo>()
+                .eq(PaymentInfo::getOrderNo, transaction.getOutTradeNo()));
+        if (paymentInfo.getPaymentStatus() == 1) {
+            return;
+        }
+
+        //更新支付信息
+        paymentInfo.setPaymentStatus(1);
+        paymentInfo.setOrderNo(transaction.getOutTradeNo());
+        paymentInfo.setTransactionId(transaction.getTransactionId());
+        paymentInfo.setCallbackTime(new Date());
+        paymentInfo.setCallbackContent(JSON.toJSONString(transaction));
+        paymentInfoMapper.updateById(paymentInfo);
+        // 表示交易成功！
+
+        // 后续更新订单状态！ 使用消息队列！
+        rabbitService.sendMessage(MqConst.EXCHANGE_ORDER, MqConst.ROUTING_PAY_SUCCESS, paymentInfo.getOrderNo());
     }
 }
